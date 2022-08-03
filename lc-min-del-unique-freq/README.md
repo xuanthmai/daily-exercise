49b7083bde4c0cb6e45456118a4c462b60410b46
Xuan Mai <55710117+xuanthmai@users.noreply.github.com>
2022-08-04 01:06:30 +0700

    [lc][min-del-unique-freq] Update README and add the implementation of a working solution

--- lc-min-del-unique-freq/README.md
+++ lc-min-del-unique-freq/README.md
@@ -1,5 +1,5 @@
 # Minimum Deletions to Make Character Frequencies Unique
-Date: 2022/08/02
+Date: 2022/08/03
 
 URL: [Leet Code - Minimum Deletions to Make Character Frequencies Unique](https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/)
 
@@ -8,13 +8,16 @@ Environment
 
 Usage:
 - `scalac solution.scala`
-- `scala MinDel`
-- Update testcases in `main`
+- `scala MinDel <input_string> <on_off_logger>` (Example: `scala MinDel thisisastring true`)
+- Update testcases in `main` or pass in the params
 
 
 Submission
 | Runtime | Memory  | Notes |
 | ------- | ------  | ----  |
-| 1084ms  | 73.2 MB | faster than 37.79%, less than 71.13% |
+| 1045ms  | 84.3 MB | faster than 55.56%, less than 60.32% |
 
 
+Notes:
+- This is horrible coding... I mean, I used var then val all mixed up
+- The solution itself is not too bad, but please rework on cleaner code
--- lc-min-del-unique-freq/solution.scala
+++ lc-min-del-unique-freq/solution.scala
@@ -1,6 +1,50 @@
 object MinDel {
-  // @TODO Implement this
-  def minDeletions(s: String): Int = ???
+  var logger: Boolean = false
 
-  def main(args: Array[String]): Unit = ???
+  def log(msg: String): Unit = {
+    if (logger) println(msg)
+  }
+
+  // @TODO refactor for cleaner code pretty please
+  def minDeletions(s: String): Int = {
+    val charFreq: Array[(Char, Int)] = s.distinct.toCharArray.map(ch => (ch, s.count(_ == ch)))
+    log(s"charFreq = ${charFreq.toList}")
+    var frequencyList: Array[Int] = charFreq.map(_._2)
+    log(s"original frequencyList = ${frequencyList.toList}")
+    var deletion: Int = 0
+    frequencyList.zipWithIndex.foreach(freqWithIdx => {
+      var newFreq = freqWithIdx._1
+      while (frequencyList.filter(_ == newFreq).length > 1) {
+        newFreq -= 1
+        deletion += 1
+        log(s"newFreq = $newFreq")
+        
+        frequencyList = 
+          if (newFreq == 0) frequencyList.updated(freqWithIdx._2, -1)
+          else frequencyList.updated(freqWithIdx._2, newFreq)
+
+        log(s"updated frequencyList = ${frequencyList.toList}")
+      }
+    })
+
+    deletion
+  }
+
+  def main(args: Array[String]): Unit = {
+    // some working cases
+    // 4
+    println(minDeletions("ababccd"))
+    // 2
+    println(minDeletions("ceabaacb"))
+    // 2
+    println(minDeletions("aaabbbcc"))
+    // 0
+    println(minDeletions("aab"))
+    // 3
+    println(minDeletions("abcd"))
+
+    // scala MinDel astring false
+    logger = args(1).toBoolean
+    println(minDeletions(args(0)))
+  }
 }
